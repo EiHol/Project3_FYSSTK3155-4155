f50d9cd17f62b0745a013607166a3bfc91eb73bc
EiHol <eirik@holm1.no>
2025-10-21 22:41:22 +0200

    Added activation and cost functions + fies for FFNN and GD

---
+++ Code/FFNN.py
@@ -0,0 +1,3 @@
+"""
+This file contains an implementation of a feed forward neural network
+"""
---
+++ Code/activation_functions.py
@@ -0,0 +1,35 @@
+"""
+This file contains various activation functions and some of their derivatives used in machine learning
+"""
+
+# Import libraries
+import numpy as np
+
+# ReLU function
+def ReLU(z):
+    return np.where(z > 0, z, 0)
+
+# Derivative of the ReLU function
+def ReLU_der(z):
+    return np.where(z > 0, 1, 0)
+
+# Leaky ReLU function
+def leaky_ReLU(z, a):
+    return np.where(z > 0, z, z * a)
+
+# Derivative of the leaky ReLU function?
+def leaky_ReLU_der(z, a):
+    return np.where(z > 0, 1, a)
+
+# Sigmooid function
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# Derivative of the sigmoid function
+def sigmoid_der(z):
+    return (np.exp(-z) / (1 + np.exp(-z))**2)
+
+# Softmax function
+def softmax(z):
+    e_z = np.exp(z - np.max(z, axis=0))
+    return e_z / np.sum(e_z, axis=1)[:, np.newaxis]
