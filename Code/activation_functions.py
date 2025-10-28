92821a6df484db50ad2f919d357c952e43c331df
EiHolm <eirik@holm1.no>
2025-10-28 22:06:14 +0100

    Moved mrs and mrs_der functions

--- Code/activation_functions.py
+++ Code/activation_functions.py
@@ -37,34 +37,8 @@ def softmax(z):
     e_z = np.exp(z - np.max(z, axis=0))
     return e_z / np.sum(e_z, axis=1)[:, np.newaxis]
 
-# Mean Squared Error with optional L1 and L2 regularization
-def mse(y_true, y_pred, weights=None, l1_lambda=0, l2_lambda=0):
-    # Mean squared error
-    mse = np.mean((y_true - y_pred) ** 2)
-
-    # Regularization terms
-    l1_penalty = 0
-    l2_penalty = 0
-    if weights is not None:
-        l1_term = l1_lambda * np.sum(np.abs(weights))
-        l2_term = l2_lambda * np.sum(weights ** 2)
-
-    return mse + l1_penalty + l2_penalty
-
-
 # Used for output layer for regression
 def identity(x):
     return x
 
-# Derivative of  Mean Squared Error with optional L1 and L2 regularization
-def mse_der(y_true, y_pred, weights=None, l1_lambda=0.0, l2_lambda=0.0):
-    # Derivative w.r.t predictions, where a = y_pred
-    dC_da = 2 * (y_pred - y_true) / y_true.size
 
-    # Derivative w.r.t weights - regularization only
-    dC_dw = None
-    if weights is not None:
-        # sign(weights) gives the subgradient for L1
-        dC_dw = l1_lambda * np.sign(weights) + 2 * l2_lambda * weights
-
-    return dC_da, dC_dw
