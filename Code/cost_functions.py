5a52befb5691fc0af9a539343ddd57c867ac6162
EiHolm <eirik@holm1.no>
2025-10-28 22:00:19 +0100

    test

--- Code/cost_functions.py
+++ Code/cost_functions.py
@@ -5,21 +5,33 @@ This file contains various cost functions and some of their derivatives used in 
 # Import libraries
 import autograd.numpy as np
 
-# Mean squared error function
-def mse(predict, target):
-    return np.mean((predict - target) ** 2)
+# Mean Squared Error with optional L1 and L2 regularization
+def mse(y_true, y_pred, weights=None, l1_lambda=0, l2_lambda=0):
+    # Mean squared error
+    mse = np.mean((y_true - y_pred) ** 2)
 
-# Derivative of the mse function
-def mse_der(predict, target):
-    return (predict - target) * (2/len(target))
+    # Regularization terms
+    l1_penalty = 0
+    l2_penalty = 0
+    if weights is not None:
+        l1_term = l1_lambda * np.sum(np.abs(weights))
+        l2_term = l2_lambda * np.sum(weights ** 2)
 
-# Mean squared error L1 norm
-def mse_L1(predict, target, lmbda):
-    pass
+    return mse + l1_penalty + l2_penalty
 
-# Mean squared error L2 norm
-def mse_L2(predict, target, lmbda):
-    pass
+
+# Derivative of  Mean Squared Error with optional L1 and L2 regularization
+def mse_der(y_true, y_pred, weights=None, l1_lambda=0.0, l2_lambda=0.0):
+    # Derivative w.r.t predictions, where a = y_pred
+    dC_da = 2 * (y_pred - y_true) / y_true.size
+
+    # Derivative w.r.t weights - regularization only
+    dC_dw = None
+    if weights is not None:
+        # sign(weights) gives the subgradient for L1
+        dC_dw = l1_lambda * np.sign(weights) + 2 * l2_lambda * weights
+
+    return dC_da, dC_dw
 
 # Binary cross entropy
 def binary_cross_entropy():
