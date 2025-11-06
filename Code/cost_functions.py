59c3c0e96884791a3fb9b5bad7e05d8e6b59cc66
Niklas Vestskogen <nives0441@oslomet.no>
2025-11-06 16:33:39 +0100

    Restore deleted FFNN_regression_testing

--- Code/cost_functions.py
+++ Code/cost_functions.py
@@ -33,6 +33,10 @@ def mse_der(y_true, y_pred, weights=None, l1_lambda=0.0, l2_lambda=0.0):
 
     return dC_da, dC_dw
 
+# Cross entropy function
+def cross_entropy(predict, target):
+    return -np.sum(target * np.log(predict))
+
 # Binary cross entropy with L1 norm
 def cross_entropy_L1(predict, target, weights, lmbda=0.01):
     
@@ -50,10 +54,6 @@ def cross_entropy_L2(predict, target, weights, lmbda=0.01):
         l2_penalty += np.sum(W**2)
     
     return cross_entropy(predict, target) + lmbda * l2_penalty
-
-# Cross entropy function
-def cross_entropy(predict, target):
-    return np.sum(-target * np.log(predict))
 
 # Softmax cross entropy derivative
 def cross_entropy_der(predict, target):
