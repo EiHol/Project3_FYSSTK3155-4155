5bbc4e15e2b89f855847dd4c136c246c74bc3ff7
EiHol <eirik@holm1.no>
2025-12-18 12:42:18 +0100

    merged with 'CNN'

---
+++ Code/cost_functions.py
@@ -0,0 +1,60 @@
+"""
+This file contains various cost functions and some of their derivatives used in machine learning.
+"""
+
+# Import libraries
+import autograd.numpy as np
+
+# Mean Squared Error with optional L1 and L2 regularization
+def mse(y_true, y_pred, weights=None, l1_lambda=0, l2_lambda=0):
+    # Mean squared error
+    MSE = np.mean((y_true - y_pred) ** 2)
+
+    # Regularization terms
+    l1_penalty = 0
+    l2_penalty = 0
+    if weights is not None:
+        l1_term = l1_lambda * np.sum(np.abs(weights))
+        l2_term = l2_lambda * np.sum(weights ** 2)
+
+    return MSE + l1_penalty + l2_penalty
+
+
+# Derivative of  Mean Squared Error with optional L1 and L2 regularization
+def mse_der(y_true, y_pred, weights=None, l1_lambda=0.0, l2_lambda=0.0):
+    # Derivative w.r.t predictions, where a = y_pred
+    dC_da = 2 * (y_pred - y_true) / y_true.size
+
+    # Derivative w.r.t weights - regularization only
+    dC_dw = None
+    if weights is not None:
+        # sign(weights) gives the subgradient for L1
+        dC_dw = l1_lambda * np.sign(weights) + 2 * l2_lambda * weights
+
+    return dC_da, dC_dw
+
+# Cross entropy function
+def binary_cross_entropy(predict, target, weights=None, L1_lambda=0, L2_lambda=0):
+    BCE = -np.mean(target * np.log(predict) + (1 - target) * np.log(1 - predict))
+
+    # Regularization terms
+    l1_penalty = 0
+    l2_penalty = 0
+    if weights is not None:
+        l1_penalty = L1_lambda * np.sum(np.abs(weights))
+        l2_penalty = L2_lambda * np.sum(weights ** 2)
+
+    return BCE + l1_penalty + l2_penalty
+
+# Cross entropy function
+def cross_entropy(predict, target, weights=None, L1_lambda=0, L2_lambda=0):
+    CE = -np.sum(target * np.log(predict))
+
+    # Regularization terms
+    l1_penalty = 0
+    l2_penalty = 0
+    if weights is not None:
+        l1_penalty = L1_lambda * np.sum(np.abs(weights))
+        l2_penalty = L2_lambda * np.sum(weights ** 2)
+
+    return CE + l1_penalty + l2_penalty
